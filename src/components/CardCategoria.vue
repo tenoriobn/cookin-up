9fc8b9756a91f560e1a71a6d1e38d92bdfff12f5
Bruno Tenório <tenoriobn@gmail.com>
2024-02-27 15:57:27 -0300

    Reutilizando estilos

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
 import type ICategoria from '@/interfaces/ICategoria';
 import type { PropType } from 'vue';
+import Tag from './Tag.vue';
 
   export default {
     props: {
-      categoria: { type: Object as PropType<ICategoria>, required: true}
-    }
-  }
+        categoria: { type: Object as PropType<ICategoria>, required: true }
+    },
+    components: { Tag }
+}
 </script>
 
 <template>
@@ -19,7 +21,7 @@ import type { PropType } from 'vue';
 
     <ul class="categoria__ingredientes">
       <li v-for="ingrediente in categoria.ingredientes" :key="ingrediente">
-        {{ ingrediente }}
+        <Tag :texto="ingrediente" />
       </li>
     </ul>
   </article>
--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,7 +1,8 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue'
+import Tag from './Tag.vue';
   export default {
-  components: { SelecionarIngredientes },
+  components: { SelecionarIngredientes, Tag },
     data () {
       return {
         ingredientes: ['Alho', 'Manteiga', 'Orégano']
@@ -18,8 +19,8 @@ import SelecionarIngredientes from './SelecionarIngredientes.vue'
       </span>
 
       <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">
-          {{ ingrediente }}
+        <li v-for="ingrediente in ingredientes" :key="ingrediente">
+          <Tag :texto="ingrediente" />
         </li>
       </ul>
 
@@ -58,18 +59,6 @@ import SelecionarIngredientes from './SelecionarIngredientes.vue'
     justify-content: center;
     gap: 1rem 1.5rem;
     flex-wrap: wrap;
-  }
-
-  .ingrediente {
-    display: inline-block;
-    border-radius: 0.5rem;
-    min-width: 4.25rem;
-    padding: 0.5rem;
-    text-align: center;
-      transition: 0.2s;
-      color: var(--creme, #FFFAF3);
-    background: var(--coral, #F0633C);
-    font-weight: 700;
   }
 
   .lista-vazia {
