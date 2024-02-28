da5cb05eb0c622b4fb4a2732179a68956d333f27
Bruno Tenório <tenoriobn@gmail.com>
2024-02-28 15:41:27 -0300

    Mantendo um componente vivo com KeepAlive

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -30,16 +30,19 @@ import SuaLista from './SuaLista.vue';
 <template>
   <main class="conteudo-principal">
     <SuaLista :ingredientes="ingredientes" />
-    <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
-      @adicionar-ingrediente="adicionarIngrediente"
-      @remover-ingrediente="removerIngrediente"
-      @buscar-receitas="navegar('MostrarReceitas')"
-    />
 
-    <MostrarReceitas 
-      v-else-if="conteudo === 'MostrarReceitas'" 
-      @editar-receitas="navegar('SelecionarIngredientes')"
-    />
+    <KeepAlive include="SelecionarIngredientes">
+      <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+        @adicionar-ingrediente="adicionarIngrediente"
+        @remover-ingrediente="removerIngrediente"
+        @buscar-receitas="navegar('MostrarReceitas')"
+      />
+  
+      <MostrarReceitas 
+        v-else-if="conteudo === 'MostrarReceitas'" 
+        @editar-receitas="navegar('SelecionarIngredientes')"
+      />
+    </KeepAlive>
   </main>
 </template>
 
